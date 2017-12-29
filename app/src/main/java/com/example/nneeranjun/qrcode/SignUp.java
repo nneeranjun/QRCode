6062b58260804046fa9e1ba904472ffed2a14f1a
nneeranjun <nilayneeranjun24@gmail.com>
2017-12-29 02:14:22 -0500

    hello

--- app/src/main/java/com/example/nneeranjun/qrcode/SignUp.java
+++ app/src/main/java/com/example/nneeranjun/qrcode/SignUp.java
@@ -13,13 +13,14 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     EditText password;
     EditText email;
@@ -27,15 +28,13 @@ public class Login extends AppCompatActivity {
     AlertDialog.Builder alertDialog;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
         firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
         password = (EditText) findViewById(R.id.password);
-        email = (EditText) findViewById(R.id.email);
+        email = (EditText)findViewById(R.id.email);
         spinner = (ProgressBar) findViewById(R.id.spinner);
         spinner.setVisibility(View.GONE);
         alertDialog = new AlertDialog.Builder(getApplicationContext());
@@ -44,14 +43,10 @@ public class Login extends AppCompatActivity {
 
 
 
-
-
     }
-
-    public void login(Context context){
+    public void signUp(View view){
         spinner.setVisibility(View.VISIBLE);
-
-        firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+        firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -59,26 +54,22 @@ public class Login extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             spinner.setVisibility(View.GONE);
                             FirebaseUser user = firebaseAuth.getCurrentUser();
-                            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                            Intent intent = new Intent(getApplicationContext(),UserCreation.class);
                             startActivity(intent);
-                            //updateUI(user);
-                        } else {
-                            spinner.setVisibility(View.GONE);
-                            alertDialog.setTitle("Error").setMessage("Invalid information").create();
 
-                            //updateUI(null);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            spinner.setVisibility(View.GONE);
+                            alertDialog.setTitle("Error").setMessage("Unable to signup").create();
+
                         }
 
                         // ...
                     }
                 });
-
-
     }
-    public void goToSignUp(View view){
-        Intent intent = new Intent(getApplicationContext(),SignUp.class);
+    public void goToLogin(View view){
+        Intent intent = new Intent(getApplicationContext(),Login.class);
         startActivity(intent);
-
     }
 }
-
